5f519d7e2588ab4b65f1c47193a19e01d2511131
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-08-19 14:16:44 -0400

    Modified and update labfile 3

--- Labfiles/03-create-plugins/C-sharp/Program.cs
+++ Labfiles/03-create-plugins/C-sharp/Program.cs
@@ -26,6 +26,7 @@ string endpoint = config["AzureAIFoundry:AIModel:Uri"]!;
 string apiKey = config["AzureAIFoundry:AIModel:ApiKey"]!;
 
 // Create a kernel with Azure OpenAI chat completion
+// ---------------------------------------------------------------
 var kernelBuilder = Kernel.CreateBuilder();
 kernelBuilder.AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
 
@@ -33,19 +34,33 @@ var kernel = kernelBuilder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
 // Add the plugin to the kernel
+// ---------------------------------------------------------------
 kernel.Plugins.AddFromType<FlightBookingPlugin>("FlightBookingPlugin");
 
 // Configure function choice behavior
+// ---------------------------------------------------------------
+
+//Option 1 - Let OpenAI decide which Functions to use for the plugin.
+//OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+//{
+//    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+//};
+
+//Option 2 - We specify the functions we when the plugins to use. Note here we only provide the search kernel function and not the book flight. So our plugin does not have the capabilities.
+KernelFunction searchFlights = kernel.Plugins.GetFunction("FlightBookingPlugin", "search_flights");
+
 OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
 {
-    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Required(functions: [searchFlights]),
 };
-
 
 var history = new ChatHistory();
 history.AddSystemMessage("The year is 2025 and the current month is January");
 
 AddUserMessage("Find me a flight to Tokyo on the 19");
+
+//Just add to narrow down repsponse
+history.AddSystemMessage("If the assistant does not have the ability to book. Just specify that it does not have the capbilities to book.");
 await GetReply();
 GetInput();
 await GetReply();
